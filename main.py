6df7e53a78b075ae7240af1e61da643a28c6525d
Daniel Curtin <dcurtin.work@gmail.com>
2024-02-26 12:26:20 -0700

    Add replayability and coverage for user error

--- main.py
+++ main.py
@@ -3,31 +3,36 @@ import random
 choices = ["Rock", "Paper", "Scissors"]
 
 def rock_paper_scissors():
+  print()
   player_choice = input("Awesome! Make your move and I'll make mine: ")
   computer_choice = choices[random.randint(0, 2)]
   
   if(player_choice.upper() == "ROCK" and computer_choice == "Rock"):
-    print(f"We tied! I also chose {computer_choice}!")
+    print(f"We tied! I also chose {computer_choice}!\n")
   elif(player_choice.upper() == "ROCK" and computer_choice == "Paper"):
-    print(f"Sorry! Since I chose {computer_choice}, you lose!")
+    print(f"Sorry! Since I chose {computer_choice}, you lose!\n")
   elif(player_choice.upper() == "ROCK" and computer_choice == "Scissors"):
-    print(f"Oh man! Since I chose {computer_choice}, you win!")
+    print(f"Oh man! Since I chose {computer_choice}, you win!\n")
   elif(player_choice.upper() == "PAPER" and computer_choice == "Rock"):
-    print(f"Oh man! Since I chose {computer_choice}, you win!")
+    print(f"Oh man! Since I chose {computer_choice}, you win!\n")
   elif(player_choice.upper() == "PAPER" and computer_choice == "Paper"):
-    print(f"We tied! I also chose {computer_choice}!")
+    print(f"We tied! I also chose {computer_choice}!\n")
   elif(player_choice.upper() == "PAPER" and computer_choice == "Scissors"):
-    print(f"Sorry! Since I chose {computer_choice}, you lose!")
+    print(f"Sorry! Since I chose {computer_choice}, you lose!\n")
   elif(player_choice.upper() == "SCISSORS" and computer_choice == "Rock"):
-    print(f"Sorry! I chose {computer_choice}, so you lose!")
+    print(f"Sorry! I chose {computer_choice}, so you lose!\n")
   elif(player_choice.upper() == "SCISSORS" and computer_choice == "Paper"):
-    print(f"Oh man! Since I chose {computer_choice}, you win!")
+    print(f"Oh man! Since I chose {computer_choice}, you win!\n")
   elif(player_choice.upper() == "SCISSORS" and computer_choice == "Scissors"):
-    print(f"We tied! I also chose {computer_choice}!")
+    print(f"We tied! I also chose {computer_choice}!\n")
+  else:
+    print("Nice try, that's not a legal move!\n")
 
-  input("Want to play again? Y/N: ")
-  
-    
+  replay = input("Want to play again? Y/N: ")
+  if(replay.upper() == "Y" or replay.upper() == "YES"):
+    rock_paper_scissors()
+  else:
+    print("That was fun, Have a great day!")
 
 player = input("Hi! Want to play a game of Rock Paper Scissors? Y/N: ")
 
